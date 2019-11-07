6e33eb11cbeb908e68ce33be9517b96952d85266
MoritzEichhorn <moritz.eichhorn@outlook.com>
2019-11-07 22:19:06 +0100

    Removed useless "throws"

--- SimpleShell/src/simpleshell/SimpleShell.java
+++ SimpleShell/src/simpleshell/SimpleShell.java
@@ -18,7 +18,7 @@ public class SimpleShell {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) throws IOException, InterruptedException {
+    public static void main(String[] args){
         boolean stay = true;
         while (stay) {
             try {
