f0800bcd82b428940bc718bfbbc309cbbaf90cf2
MoritzEichhorn <moritz.eichhorn@outlook.com>
2019-11-14 22:58:18 +0100

    Fixed simple_shell.c

--- simple_shell.c
+++ simple_shell.c
@@ -7,26 +7,35 @@
 #include <sys/wait.h>
 
 char **convert_to_array(char *);
+int get_next_position(char* input);
 
 int main() {
     int status;
     char **command;
     char input[64];
+    char inputCpy[64];
     char* part;
+    char del[2] = "&";
+    
     printf("Enter command: ");
     scanf("%[^\n]s", input);
-    part = strtok(input, "&");
+    
+    strcpy(inputCpy, input + get_next_position(input));
+    part = strtok(input, del);
     while(part != 0) {
         command = convert_to_array(part);
          if (fork() != 0) {
             waitpid(-1, &status, 0);
+            strcpy(input, inputCpy);
+            strcpy(inputCpy, inputCpy + get_next_position(inputCpy));
+            part = strtok(input, del);
+            
         } else {
             execvp(command[0], command);
+            part = 0;
         }    
-        part = strtok(0, "&");
         free(command);
     }
-
     free(command);
     
     return 0;
@@ -46,4 +55,13 @@ char **convert_to_array(char *input) {
 
     command[i] = 0;
     return command;
+}
+
+int get_next_position(char* input) {
+    for(int i = 0; i < strlen(input); i++) {
+        if(input[i] == '&') {
+            return i + 1;
+        }
+    }
+    return -1;
 }
